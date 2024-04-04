de6c3e2a31a711a76656b3c4159839f005e391c0
Furkan <furkanilaslan@gmail.com>
2024-04-04 23:59:56 +0300

    second commit

--- otoGaleri/settings.py
+++ otoGaleri/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x^l2rde-ee(d8a-v^*=vx8u(q)&fc#7w_^r6mxd@u81y=o7zl6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["otogaleri.pythonanywhere.com"]
 
 
 # Application definition
@@ -124,6 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
